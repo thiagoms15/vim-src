1ae722f79ae53ba08a13dce70795fa7d3a16fe62
Thiago Marques <thiagoms.15@gmail.com>
2026-06-21 19:28:03 +0100

    Fix error in treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,7 +8,7 @@ return {
         'nvim-treesitter/nvim-treesitter-context'
     },
     config = function()
-        require("nvim-treesitter.configs").setup({
+        require("nvim-treesitter.config").setup({
             sync_install = false,
             ignore_install = { },
             modules = { },
